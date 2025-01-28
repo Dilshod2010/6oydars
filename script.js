ecaf59f871e7cfb4be95ace76a8559504067d8ea
Dilshod2010 <dilshodjon.fahriddinov@gmail.com>
2025-01-28 19:17:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const ism = prompt("Ismingizni kiriting:");
+const familiya = prompt("Familiyangizni kiriting:");
+const yosh = prompt("Yoshingizni kiriting:");
+const yashashJoy = prompt("Qayerda yashaysiz?");
+const yoqtirganFan = prompt("Eng yoqtirgan fanningiz qanday?");
+
+
+console.log("Foydalanuvchi ma'lumotlari:");
+console.log(`Ism: ${ism}`);
+console.log(`Familiya: ${familiya}`);
+console.log(`Yosh: ${yosh}`);
+console.log(`Yashash joyi: ${yashashJoy}`);
+console.log(`Yoqtirgan fan: ${yoqtirganFan}`);
